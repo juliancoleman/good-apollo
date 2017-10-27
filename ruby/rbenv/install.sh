f3f6b7618bad7f5442203264dfb9c9a0f47f27eb
J. Coleman <jlcoleman3@pipeline.sbcc.edu>
2017-10-27 00:59:42 -0700

    Update install.sh

--- ruby/rbenv/install.sh
+++ ruby/rbenv/install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+sudo apt install -y libssl-dev libreadline-dev zlibig-dev
+
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
