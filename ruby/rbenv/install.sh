fa004e05a9b2b2f2d93caac479f5ab199f98fcde
J. Coleman <jlcoleman3@pipeline.sbcc.edu>
2017-10-27 13:39:48 -0700

    silence apt installs

--- ruby/rbenv/install.sh
+++ ruby/rbenv/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt install -y libssl-dev libreadline-dev zlib1g-dev
+sudo apt -qq install -y libssl-dev libreadline-dev zlib1g-dev
 
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
